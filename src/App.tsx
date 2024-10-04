704c218ba5257c28a5ad83396efdfc2c45755e43
Omi <bell.tsaeb.shoji+work@gmail.com>
2024-10-05 02:22:00 +0900

    三目並べに課題を変更しました

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,58 @@
-import { ChakraProvider } from "@chakra-ui/react";
+// import React, { useState } from "react";
 
-function App() {
-  return <ChakraProvider></ChakraProvider>;
-}
+import { useState } from "react";
 
-export default App;
+type squareProps = {
+  value: string | null;
+  onSquareClick: () => void;
+};
+
+const Square: React.FC<squareProps> = ({ value, onSquareClick }) => {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+};
+
+// X>O>X>Oにする。そのためには次がどっちなのかを都度判定する必要がある。
+
+const Board = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const onSquareClick = (i: number) => {
+    const newSquares = squares.slice();
+    newSquares[i] = "X";
+    setSquares(newSquares);
+  };
+
+  return (
+    <>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => onSquareClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => onSquareClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => onSquareClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => onSquareClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => onSquareClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => onSquareClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => onSquareClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => onSquareClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => onSquareClick(8)} />
+      </div>
+    </>
+  );
+};
+
+export default Board;
+
+/*
+Squareで管理するもの => 表示の受け皿とクリックイベントの設置
+Boardで管理するもの => 次のプレーヤー、各盤面の状態
+
+----
+valueがいま一意になっている、このままだと全部Xor全部Oになる
+SuareのレンダリングのタイミングでそれぞれのSquareを別のものとして設定したい
+*/
